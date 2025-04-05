bbb169d2a9559cbb3483ec7ed00efe68b0362d6d
Sarthak Khandelwal <mastermind.sarthak@gmail.com>
2025-04-05 18:59:29 -0400

    add: more debug

--- tp_train.py
+++ tp_train.py
@@ -129,6 +129,8 @@ def main():
         eval_dataset=eval_dataset,
         data_collator=data_collator,
     )
+    for name, param in model.named_parameters():
+        print(f"{name:60} | Type: {type(param)} | Device: {param.device}")
     
     trainer.train()
 
